af6cdcc5ffce7aac2f8a6b458b367caba0e99b2e
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-08-26 00:02:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 setTimeout(async () => {
-    (Data = { ...{ Repository } });
-    (Page = { ...{ Load: function(...d) { !this.Inited && (this.Inited = true, createElement(Data.Repository.Web.Root())); updateElement(["main", { innerHTML: "" }, (r => r?.length == 0 ? [["h2", { textContent: "404" }]] : r)(d.reduce((a, v) => a?.[v], this))], ["#nav-btn", { checked: false }]); } }, ...Data.Repository.Web.Page })["Load"]("Tools", "Number System Conversion");
-}, 10);
+    (Data = { ...{ Repository: (await Database.select("data", "key=eq.Repository"))[0].value } });
+    (Page = { ...{ Load: function(...d) { !this.Inited && (this.Inited = true, CreateElement(Function(`return ${Data.Repository.Web.Root}()`)())); UpdateElement(["main", { innerHTML: "" }, (r => r?.length == 0 ? [["h2", { textContent: "404" }]] : r)(d.reduce((a, v) => a?.[v], this))], ["#nav-btn", { checked: false }]); } }, ...Function(`return ${Data.Repository.Web.Page}`)() })["Load"]("Home");
+}, 100);
