ec7c4b970d2ec92f720a347aa5da9bc38bb55b6f
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-09-20 01:03:05 +0700

    Update promocode.js

--- api/promocode.js
+++ api/promocode.js
@@ -16,18 +16,12 @@ async function Database(action, table, filter = "", data = {}) {
     const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoc29qcm12dHRvZHpzbHV0cHRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExMzQ2OTcsImV4cCI6MjAzNjcxMDY5N30.AuzooWsIxmyttMRN_eIRdG6hDQWCfGj-SPR6fmxmuvY";
     const url = `https://bhsojrmvttodzslutpto.supabase.co/rest/v1/${table}?${filter}`;
     const headers = { "apiKey": key, "Authorization": `Bearer ${key}`, "Content-Type": "application/json" };
-
     switch (action) {
-        case "select":
-            return await (await fetch(url, { method: "GET", headers })).json();
-        case "insert":
-            return await fetch(url, { method: "POST", headers, body: JSON.stringify(data) });
-        case "update":
-            return await fetch(url, { method: "PATCH", headers, body: JSON.stringify(data) });
-        case "delete":
-            return await fetch(url, { method: "DELETE", headers, body: JSON.stringify(data) });
-        default:
-            throw new Error("Invalid action");
+        case "select": return await (await fetch(url, { method: "GET", headers })).json();
+        case "insert": return await fetch(url, { method: "POST", headers, body: JSON.stringify(data) });
+        case "update": return await fetch(url, { method: "PATCH", headers, body: JSON.stringify(data) });
+        case "delete": return await fetch(url, { method: "DELETE", headers, body: JSON.stringify(data) });
+        default: throw new Error("Invalid action");
     }
 }
 
@@ -42,7 +36,7 @@ module.exports = async (req, res) => {
         req.on("end", async () => {
             try {
                 const code = JSON.parse(decrypt(decodeURIComponent(decodeURIComponent(body)).replace(/-/g, '+').replace(/_/g, '/').replace(/^data=/, ''), key)).code;
-                const data = await Database("select", "promocode", `code=eq.${code}`)[0].data;
+                const data = (await Database("select", "promocode", `code=eq.${code}`))[0].data;
                 await fetch('https://discord.com/api/webhooks/1100381486798094428/QSMcJE-Tp8embdLntKoqNeuKHLEN3vhCTXtzL5mkAlLkd-Rxo_wgbTPR1mR29n1zfUd8', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
