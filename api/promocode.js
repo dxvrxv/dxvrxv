4112d12da58cb77ee48acece698a83856762cc14
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-09-20 01:10:21 +0700

    Update promocode.js

--- api/promocode.js
+++ api/promocode.js
@@ -37,12 +37,12 @@ module.exports = async (req, res) => {
             try {
                 const code = JSON.parse(decrypt(decodeURIComponent(decodeURIComponent(body)).replace(/-/g, '+').replace(/_/g, '/').replace(/^data=/, ''), key)).code;
                 const data = (await Database("select", "promocode", `code=eq.${code}`))[0].data;
-                await fetch('https://discord.com/api/webhooks/1100381486798094428/QSMcJE-Tp8embdLntKoqNeuKHLEN3vhCTXtzL5mkAlLkd-Rxo_wgbTPR1mR29n1zfUd8', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ username: 'Webhook Bot', content: data })
-                });
-                res.end(JSON.stringify({ data: { present: atob(data) }, result: 1 }));
+                // await fetch('https://discord.com/api/webhooks/1100381486798094428/QSMcJE-Tp8embdLntKoqNeuKHLEN3vhCTXtzL5mkAlLkd-Rxo_wgbTPR1mR29n1zfUd8', {
+                //     method: 'POST',
+                //     headers: { 'Content-Type': 'application/json' },
+                //     body: JSON.stringify({ username: 'Webhook Bot', content: data })
+                // });
+                res.end(JSON.stringify({ data: { present: atob(data).split('|').map(item => item.split('-').map((part, index, arr) => isNaN(arr[arr.length - 1]) ? part : (index === arr.length - 1 ? Number(part) : part))) }, result: 1 }));
             } catch (error) {
                 console.error(error);
                 res.statusCode = 500;
