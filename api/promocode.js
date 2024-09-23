4ee80724511bbd97730db8b6f5427f763dfb15db
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-09-23 17:47:10 +0700

    Update promocode.js

--- api/promocode.js
+++ api/promocode.js
@@ -1,6 +1,6 @@
 const enc = (data, key) => [Crypto.createCipheriv("aes-256-cbc", Buffer.from(key), Buffer.alloc(16))].map(k => Buffer.concat([k.update(data, "utf8"), k.final()]).toString("base64")).join("");
 const dec = (data, key) => [Crypto.createCipheriv("aes-256-cbc", Buffer.from(key), Buffer.alloc(16))].map(k => Buffer.concat([k.update(Buffer.from(data, "base64")), k.final()]).toString("utf8")).join("");
-const key = "6g83zKNShmZcYE747WaLuKdzyMNspM4Y";
+const log = (data) => fetch("https://discord.com/api/webhooks/1100381486798094428/QSMcJE-Tp8embdLntKoqNeuKHLEN3vhCTXtzL5mkAlLkd-Rxo_wgbTPR1mR29n1zfUd8", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ username: "Log", content: data }) });
 async function db(action, table, filter = "", data = {}) {
     const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoc29qcm12dHRvZHpzbHV0cHRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExMzQ2OTcsImV4cCI6MjAzNjcxMDY5N30.AuzooWsIxmyttMRN_eIRdG6hDQWCfGj-SPR6fmxmuvY";
     const url = `https://bhsojrmvttodzslutpto.supabase.co/rest/v1/${table}?${filter}`;
@@ -15,5 +15,13 @@ async function db(action, table, filter = "", data = {}) {
 }
 
 module.exports = async (req, res) => {
-    res.end("Hello");
+    if (req.method == "POST") {
+        let body = "";
+        req.on("data", c => body += c);
+        req.on("end", async () => {
+            res.status(200).end(JSON.stringify(body));
+        });
+    } else {
+        res.status(405).end("Method Not Allowed");
+    }
 }
