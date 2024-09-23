79f6967ac7521dd9d1e38d7c80f3267e7c692564
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-09-24 00:37:22 +0700

    Update promocode.js

--- api/promocode.js
+++ api/promocode.js
@@ -1,7 +1,7 @@
 const Crypto = require("crypto");
 const enc = (data, key) => [Crypto.createCipheriv("aes-256-cbc", Buffer.from(key), Buffer.alloc(16))].map(k => Buffer.concat([k.update(data, "utf8"), k.final()]).toString("base64")).join("");
 const dec = (data, key) => [Crypto.createDecipheriv("aes-256-cbc", Buffer.from(key), Buffer.alloc(16))].map(k => Buffer.concat([k.update(Buffer.from(data, "base64")), k.final()]).toString("utf8")).join("");
-const log = (data) => fetch("https://discord.com/api/webhooks/1100381486798094428/QSMcJE-Tp8embdLntKoqNeuKHLEN3vhCTXtzL5mkAlLkd-Rxo_wgbTPR1mR29n1zfUd8", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ username: "Log", content: data }) });
+const log = async (data) => await fetch("https://discord.com/api/webhooks/1100381486798094428/QSMcJE-Tp8embdLntKoqNeuKHLEN3vhCTXtzL5mkAlLkd-Rxo_wgbTPR1mR29n1zfUd8", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ username: "Log", content: data }) });
 const key = "6g83zKNShmZcYE747WaLuKdzyMNspM4Y";
 async function db(action, table, filter = "", data = {}) {
     const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoc29qcm12dHRvZHpzbHV0cHRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExMzQ2OTcsImV4cCI6MjAzNjcxMDY5N30.AuzooWsIxmyttMRN_eIRdG6hDQWCfGj-SPR6fmxmuvY";
@@ -30,7 +30,7 @@ module.exports = async (req, res) => {
                     const data = (await db("select", "promocode", "code=eq." + code))[0];
                     if (data && data.remain > 0) {
                         db("update", "promocode", "code=eq." + code, { remain: data.remain - 1 });
-                        log("someone use " + code);
+                        await log("someone use " + code);
                         res.status(200).end(enc(JSON.stringify({ data: { present: JSON.stringify(atob(data.content).split("|").map(d => d.split("-").map(v => isNaN(v) ? v : Number(v)))) }, result: 1 }), key));
                     } else res.status(200).end("code doesn't exists or has expired");
                 } else if (body.split("=")[0] == "setdata") {
@@ -38,7 +38,7 @@ module.exports = async (req, res) => {
                     if ((await db("select", "promocode", "code=eq." + data.code))[0]) res.status(200).end("code already exists");
                     else {
                         db("insert", "promocode", "", data);
-                        log("someone create " + data.code);
+                        await log("someone create " + data.code);
                         res.status(200).end("code has created");
                     }
                 }
