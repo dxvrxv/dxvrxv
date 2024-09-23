bcf062a90060c02ff4491b25d526e487af546d7e
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-09-23 17:09:24 +0700

    Update promocode.js

--- api/promocode.js
+++ api/promocode.js
@@ -1,30 +1,22 @@
 const fetch = require("node-fetch");
-// const enc = (data, key) => [Crypto.createCipheriv("aes-256-cbc", Buffer.from(key), Buffer.alloc(16))].map(k => Buffer.concat([k.update(data, "utf8"), k.final()]).toString("base64")).join("");
-// const dec = (data, key) => [Crypto.createCipheriv("aes-256-cbc", Buffer.from(key), Buffer.alloc(16))].map(k => Buffer.concat([k.update(Buffer.from(data, "base64")), k.final()]).toString("utf8")).join("");
+const enc = (data, key) => [Crypto.createCipheriv("aes-256-cbc", Buffer.from(key), Buffer.alloc(16))].map(k => Buffer.concat([k.update(data, "utf8"), k.final()]).toString("base64")).join("");
+const dec = (data, key) => [Crypto.createCipheriv("aes-256-cbc", Buffer.from(key), Buffer.alloc(16))].map(k => Buffer.concat([k.update(Buffer.from(data, "base64")), k.final()]).toString("utf8")).join("");
 const log = (data) => fetch("https://discord.com/api/webhooks/1100381486798094428/QSMcJE-Tp8embdLntKoqNeuKHLEN3vhCTXtzL5mkAlLkd-Rxo_wgbTPR1mR29n1zfUd8", { method: "POST", headers: { "Content-Type": "application/json" }, body: { "username": "Log", "content": data } });
-// const key = "6g83zKNShmZcYE747WaLuKdzyMNspM4Y";
-// async function db(action, table, filter = "", data = {}) {
-//     const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoc29qcm12dHRvZHpzbHV0cHRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExMzQ2OTcsImV4cCI6MjAzNjcxMDY5N30.AuzooWsIxmyttMRN_eIRdG6hDQWCfGj-SPR6fmxmuvY";
-//     const url = `https://bhsojrmvttodzslutpto.supabase.co/rest/v1/${table}?${filter}`;
-//     const headers = { "apiKey": key, "Authorization": `Bearer ${key}`, "Content-Type": "application/json" };
-//     switch (action) {
-//         case "select": return await (await fetch(url, { method: "GET", headers })).json();
-//         case "insert": return await fetch(url, { method: "POST", headers, body: JSON.stringify(data) });
-//         case "update": return await fetch(url, { method: "PATCH", headers, body: JSON.stringify(data) });
-//         case "delete": return await fetch(url, { method: "DELETE", headers, body: JSON.stringify(data) });
-//         default: throw new Error("Invalid action");
-//     }
-// }
+const key = "6g83zKNShmZcYE747WaLuKdzyMNspM4Y";
+async function db(action, table, filter = "", data = {}) {
+    const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoc29qcm12dHRvZHpzbHV0cHRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExMzQ2OTcsImV4cCI6MjAzNjcxMDY5N30.AuzooWsIxmyttMRN_eIRdG6hDQWCfGj-SPR6fmxmuvY";
+    const url = `https://bhsojrmvttodzslutpto.supabase.co/rest/v1/${table}?${filter}`;
+    const headers = { "apiKey": key, "Authorization": `Bearer ${key}`, "Content-Type": "application/json" };
+    switch (action) {
+        case "select": return await (await fetch(url, { method: "GET", headers })).json();
+        case "insert": return await fetch(url, { method: "POST", headers, body: JSON.stringify(data) });
+        case "update": return await fetch(url, { method: "PATCH", headers, body: JSON.stringify(data) });
+        case "delete": return await fetch(url, { method: "DELETE", headers, body: JSON.stringify(data) });
+        default: throw new Error("Invalid action");
+    }
+}
 
 module.exports = async (req, res) => {
-    log("hi");
-    if (req.method == "POST") {
-        let body = "";
-        req.on("data", c => body += c.toString());
-        req.on("end", async () => {
-            const query = JSON.parse(body);
-            res.end(query);
-            log(body);
-        });
-    }
+    log(JSON.stringify(req));
+    res.end("Hello");
 }
