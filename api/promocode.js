2d36e835e92f36b07451c2c43ffe04bb8fc163fa
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-09-23 17:15:52 +0700

    Update promocode.js

--- api/promocode.js
+++ api/promocode.js
@@ -19,6 +19,6 @@ module.exports = async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    // log(JSON.stringify(req));
+    log(req);
     res.end("Hello");
 }
