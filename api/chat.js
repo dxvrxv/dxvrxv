b71f5d39d5722b17b72fd105fa8a490986c7992a
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-10-05 00:00:05 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-messages = [
+let messages = [
     { author: "DX 1", content: "Test 1" },
     { author: "DX 2", content: "Test 2" },
     { author: "DX 3", content: "Test 3" },
@@ -6,6 +6,11 @@ messages = [
     { author: "DX 5", content: "Test 5" },
     { author: "DX 6", content: "Test 6" }
 ];
+
 module.exports = async (req, res) => {
-  res.json(messages);
-}
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const author = url.searchParams.get('author');
+    const content = url.searchParams.get('content');
+    if (author && content) messages.push({ author, content });
+    res.json(messages);
+};
