77153def840292d4c680f00d115cbdfe2fde5891
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-10-05 20:03:16 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -10,12 +10,12 @@ const messages = [ { msgId: randomID(), channel: 1, text: "Welcome", userId: -1,
 const players = {};
 module.exports = async (req, res) => {
     const { channel, text, userName, flag } = JSON.parse(atob(new URL(req.url, `http://${req.headers.host}`).searchParams.get("data")));
-    if (players[author]) {
+    if (players[userName]) {
         if (content) {
             const msgObj = { msgId: randomID(), channel, text, userId: players.length, userName, flag: flag || 0 }
             messages.push(msgObj); Object.keys(players).forEach(p => players[p].push(msgObj));
         }
-    } else { res.end(JSON.stringify(messages)); players[author] = []; return }
-    res.end(JSON.stringify(players[author]));
-    players[author] = [];
+    } else { res.end(JSON.stringify(messages)); players[userName] = []; return }
+    res.end(JSON.stringify(players[userName]));
+    players[userName] = [];
 };
