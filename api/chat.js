e3c8bcffe746fee6bbe3a2ab651d98fd7818ebf9
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-10-13 15:06:57 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,28 +1,23 @@
-const usedIds = new Set();
-const randomID = () => {
-    if (usedIds.size >= 9999) throw new Error("All IDs used.");
-    let id;
-    do { id = Math.floor(Math.random() * 9999) + 1; } while (usedIds.has(id));
-    usedIds.add(id);
-    return id;
-};
-const messages = [ { msgId: randomID(), channel: 1, text: "Welcome", userId: -1, userName: "Server", flag: 0 } ];
-const players = {};
-const timeout = {};
-module.exports = async (req, res) => {
-    const { channel, text, userName, flag } = JSON.parse(atob(new URL(req.url, `http://${req.headers.host}`).searchParams.get("data")));
-    if (players[userName]) {
-        if (text) {
-            const msgObj = { msgId: randomID(), channel, text, userId: players.length, userName, flag: flag || 0, players: Object.keys(players).length }
-            messages.push(msgObj); Object.keys(players).forEach(p => players[p].push(msgObj));
-        }
-    } else { res.end(JSON.stringify(messages)); players[userName] = []; return }
-    clearTimeout(timeout[userName]);
-    timeout[userName] = setTimeout(() => {
-        delete players[userName];
-        delete timeout[userName];
-    }, 20000);
-    res.end(JSON.stringify(players[userName]));
-    players[userName] = [];
-};
+const server = { online: 0, messages: [
+    { id: 0, text: "Welcome", channel: 1, uid: -1, name: "Server", role: 0 }
+] };
+const player = {};
 
+module.exports = async(req, res) => {
+    const { channel, text, name, uid } = JSON.parse(atob(new URL(req.url, `http://${req.headers.host}`).searchParams.get("data")));
+    if (player[name] && player[name].isOnline) {
+        if (text) {
+            const msgId = server.messages.length;
+            server.messages.push({ id: msgId, text, channel, name, uid, role: player[name].role || 0 });
+            Object.keys(player).forEach(p => player[p].push(msgId));
+        }
+    } else {
+        res.end(JSON.stringify(server.messages));
+        player[name] = { messages: [[], []], isOnline: true, role: name == "dxvrxv" ? 1 : 0 }
+        return
+    }
+    clearTimeout(player[name].timeout);
+    player[name].timeout = setTimeout(() => player[name].isOnline = false, 15000);
+    res.end(JSON.stringify({ online: server.online, messages: server.messages.filter(msg => player[name].messages[0].includes(msg.id)) }));
+    player[name].messages[0] = [];
+}
