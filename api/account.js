0d986c8b1440e745e2e43f8f00ef8e6380cd3098
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-10-20 02:06:19 +0700

    Update account.js

--- api/account.js
+++ api/account.js
@@ -6,7 +6,7 @@ const player = {
 module.exports = (req, res) => {
     const { userid, position, armor } = JSON.parse(atob(new URL(req.url, `http://${req.headers.host}`).searchParams.get("data")));
     if (player[userid]) {
-        if (x && y && armor) Object.assign(player[userid], { x, y, armor });
+        if (position && armor) Object.assign(player[userid], { position, armor });
         res.json(player[userid]);
     }
     else res.json({ message: "User ID not found" });
