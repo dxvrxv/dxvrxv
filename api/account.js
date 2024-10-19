b6e6ac6d73ab6b2f77e61fa2658b27d45807c409
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-10-20 03:58:12 +0700

    Update account.js

--- api/account.js
+++ api/account.js
@@ -1,14 +1,14 @@
 const server = {};
 const player = {
-    [1337]: { userid: 1337, username: "dxvrxv", online: false, position: {}, armor: { id: "" }, health: {} },
-    [1000]: { userid: 1000, username: "Bot", online: false, position: { y: 1.6063527708178, x: 4.411156398605 }, armor: { id: "ozk" }, health: {} }
+    [1337]: { userid: 1337, username: "dxvrxv", online: false, position: {}, armorid: "", health: {} },
+    [1000]: { userid: 1000, username: "Bot", online: false, position: { y: 1.6063527708178, x: 4.411156398605 }, armorid: "ozk", health: {} }
 };
 
 module.exports = (req, res) => {
-    const { userid, position, armor } = JSON.parse(atob(new URL(req.url, `http://${req.headers.host}`).searchParams.get("data")));
+    const { userid, position, armorid } = JSON.parse(atob(new URL(req.url, `http://${req.headers.host}`).searchParams.get("data")));
     if (player[userid]) {
-        if (position && armor) {
-            Object.assign(player[userid], { position, armor });
+        if (position && armorid) {
+            Object.assign(player[userid], { position, armorid });
             res.json(player);
         } else res.json(player[userid]);
     }
