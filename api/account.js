2d1c698604afdfc3d317f0016ffe0e24570c7381
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-10-20 11:31:26 +0700

    Update account.js

--- api/account.js
+++ api/account.js
@@ -1,7 +1,7 @@
 const server = {};
 const player = {
     [1337]: { userid: 1337, username: "dxvrxv", online: false, position: { x: 0, y: 0 }, armorid: "", health: {} },
-    [1000]: { userid: 1000, username: "Bot", online: false, position: { y: 1.6063527708178, x: 4.411156398605 }, armorid: "ozk", hp: 100, armor: 100, armordep: 0, weaponlist: [{id: "ak47_gold"}] }
+    [1000]: { userid: 1000, username: "Bot", online: false, position: { y: 1.6063527708178, x: 4.411156398605 }, armorid: "ozk", hp: 100, armor: 100, armordep: 0, armorLoss: 1, weaponlist: [{id: "ak47_gold"}] }
 };
 
 module.exports = (req, res) => {
