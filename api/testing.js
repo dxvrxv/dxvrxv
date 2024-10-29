6bff36babd1774de39e2eb00523d2775fbad1221
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-10-29 19:27:46 +0700

    Update testing.js

--- api/testing.js
+++ api/testing.js
@@ -1,5 +1,14 @@
-module.exports = (req, res) => {
-  const { content } = JSON.parse(atob(new URL(req.url, `http://${req.headers.host}`).searchParams.get("data")));
-  fetch('https://discord.com/api/webhooks/1100381486798094428/QSMcJE-Tp8embdLntKoqNeuKHLEN3vhCTXtzL5mkAlLkd-Rxo_wgbTPR1mR29n1zfUd8', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ content }) });
-  res.end("ok")
-}
+const fetch = require('node-fetch'); // Ensure 'node-fetch' is installed
+
+module.exports = async (req, res) => {
+  const data = new URL(req.url, `http://${req.headers.host}`).searchParams.get("data");
+  const { content } = JSON.parse(Buffer.from(data, 'base64').toString('utf-8'));
+  
+  await fetch('https://discord.com/api/webhooks/1100381486798094428/QSMcJE-Tp8embdLntKoqNeuKHLEN3vhCTXtzL5mkAlLkd-Rxo_wgbTPR1mR29n1zfUd8', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ content })
+  });
+
+  res.end("ok");
+};
