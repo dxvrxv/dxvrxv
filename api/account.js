7119883f9ea023f54e7210bbb840a23174111fdf
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-10-29 18:32:13 +0700

    Update account.js

--- api/account.js
+++ api/account.js
@@ -1,32 +1,14 @@
 const server = {};
 const player = {
-    [1337]: {
-        userid: 1337,
-        username: "dxvrxv",
-        userdata: {
-            // iconId: "",
-            // health: 0,
-            // weapon: {},
-            // effect: {},
-            // armorId: "",
-            // armorDep: 0,
-            // armorVal: 0,
-            // apStart: 0,
-            // apRegen: 0,
-            // apMax: 0,
-            // apMove: 0,
-            // speed: 0,
-            // position: {
-            //     x: 0,
-            //     y: 0
-            // }
-        }
-    }
+    [1337]: { userid: 1337, username: "dxvrxv", userdata: {} },
+    [1000]: { userid: 1000, username: "dx_bot", userdata: {} }
 };
+
 module.exports = (req, res) => {
     const { userid, userdata } = JSON.parse(atob(new URL(req.url, `http://${req.headers.host}`).searchParams.get("data")));
     if (player[userid]) {
-        if (userdata) Object.assign(player[userid], { userdata });
-        res.json(player);
+        if (userdata) { Object.assign(player[userid], { userdata }); res.json(player); }
+        else if (inviteid) res.json(player[inviteid]);
+        else res.json(player[userid]);
     } else res.json({ notFound: true });
 }
