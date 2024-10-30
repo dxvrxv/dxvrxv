0412416db3a13a76f9ff65630f6902d0b603a670
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-10-31 00:54:18 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,4 @@
-const server = {
-    online: 0,
-    messages: [],
-    privates: {}
-};
+const server = { online: 0, messages: [] };
 const player = {
     [1337]: {
         userid: 1337,
@@ -10,42 +6,43 @@ const player = {
         username: "dxvrxv",
         isOnline: false,
         gamedata: {
-            position: [0, 0],
-            name: "",
+            position: [0, 0]
         },
-        messages: []
+        messages: [],
+        timeout: null
     }
 };
 
 module.exports = (req, res) => {
-    
     const { userid, helpid, gamedata, content, channel } = JSON.parse( atob( new URL( req.url, `http://${ req.headers.host }` ).searchParams.get( "data" ) ) );
-    
+
     if ( userid ) {
-        
-        if ( content && channel ) {
-            const messageId = server.messages.length;
-            server.messages.push( { userid, messageId, channel, content } );
-            Object.keys( player ).forEach( uid => player[ uid ].messages.push( messageId ) );
-        } else if ( gamedata ) Object.assign( player[ userid ], { gamedata } );
 
-        res.json( {
-            server: { online: server.online, messages: server.messages.filter( chat => player[ userid ].messages.includes( chat.messageId ) ) },
-            player: Object.values( player ).map( p => ( { userid: p.userid, username: p.username, gamedata: p.gamedata } ) ),
-            ...( helpid && { helper: { gamedata: player[ helpid ].gamedata } } )
-        } );
+        if ( player[ userid ].isOnline ) {
+            if ( content && channel ) {
+                const messageId = server.messages.length;
+                server.messages.push( { userid, messageId, channel, content } );
+                Object.keys( player ).forEach( uid => player[ uid ].messages.push( messageId ) );
+            } else if ( gamedata ) Object.assign( player[ userid ], { gamedata } );
+    
+            clearTimeout( player[ userid ].timeout );
+            player[ userid ].timeout = setTimeout( () => player[ userid ].isOnline = false );
+    
+            res.json( {
+                server: { online: server.online, messages: server.messages.filter( chat => player[ userid ].messages.includes( chat.messageId ) ) },
+                player: Object.values( player ).map( p => ( { userid: p.userid, username: p.username, gamedata: { armorIconId: p.gamedata?.armorIconId || "", position: p.gamedata.position } } ) ),
+                ...( helpid && { helper: { gamedata: player[ helpid ].gamedata } } )
+            } );
+    
+            player[ userid ].messages = [];
 
-        player[ userid ].messages = [];
+        } else {
+
+            player[ userid ].isOnline = true;
+            res.json( { server, player: Object.values( player ).map( p => ( { userid: p.userid, username: p.username, gamedata: { armorIconId: p.gamedata?.armorIconId || "", position: p.gamedata.position } } ) ) } )
+            
+        }
 
     } else res.json( { notFound: true } );
-}
 
-/*
-api?data={userid, content, channel}
-    { server: { online, messages } }
-api?data={userid, gamedata}
-    { server: { online, messages }, player: { gamedata } }
-api?data={userid, helpid}
-    { helper: { gamedata } }
-api?data={userid}
-*/
+}
