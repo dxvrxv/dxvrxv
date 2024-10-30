6d0b31b2ec26fcb6a97c676a22237aac91083d77
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-10-31 01:10:15 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,19 +1,23 @@
 const server = { online: 0, messages: [] };
 const player = {
-    [1337]: {
-        userid: 1337,
-        access: 0,
-        username: "dxvrxv",
-        isOnline: false,
-        gamedata: {
-            position: [0, 0]
-        },
-        messages: [],
-        timeout: null
-    }
+    [1337]: { userid: 1337, access: 0, username: "dxvrxv", isOnline: false, gamedata: { position: [0, 0] }, messages: [], timeout: null },
+    [1000]: { userid: 1000, access: 0, username: "dx_bot", isOnline: false, gamedata: { position: [0, 0] }, messages: [], timeout: null },
 };
 
-module.exports = (req, res) => {
+async function db(action, table, filter = "", data = {}) {
+    const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoc29qcm12dHRvZHpzbHV0cHRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExMzQ2OTcsImV4cCI6MjAzNjcxMDY5N30.AuzooWsIxmyttMRN_eIRdG6hDQWCfGj-SPR6fmxmuvY";
+    const url = `https://bhsojrmvttodzslutpto.supabase.co/rest/v1/${table}?${filter}`;
+    const headers = { "apiKey": key, "Authorization": `Bearer ${key}`, "Content-Type": "application/json" };
+    switch (action) {
+        case "select": return await (await fetch(url, { method: "GET", headers })).json();
+        case "insert": return await fetch(url, { method: "POST", headers, body: JSON.stringify(data) });
+        case "update": return await fetch(url, { method: "PATCH", headers, body: JSON.stringify(data) });
+        case "delete": return await fetch(url, { method: "DELETE", headers, body: JSON.stringify(data) });
+        default: throw new Error("Invalid action");
+    }
+}
+
+module.exports = async (req, res) => {
     const { userid, helpid, gamedata, content, channel } = JSON.parse( atob( new URL( req.url, `http://${ req.headers.host }` ).searchParams.get( "data" ) ) );
 
     if ( userid ) {
