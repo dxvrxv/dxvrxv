5dc8b0ddc7e71dabde226be1a5cadd3d68bcaf7d
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2024-11-18 21:19:45 +0700

    Update account.js

--- api/account.js
+++ api/account.js
@@ -1,6 +1,24 @@
 const server = {};
 const player = {
-    [1337]: { userid: 1337, username: "dxvrxv", userdata: {} },
+    [1337]: { userid: 1337, username: "dxvrxv", userdata: {
+        position: { y: 1.6074578934414, x: 4.4045622496041 },
+        china_season: 10,
+        emba_event: 10,
+        wearWeaponList: [ 'ks23', 'muha', 'shmel' ],
+        apMax: 5,
+        apRegen: 1,
+        armorId: 'tactical_armor',
+        hp: 100,
+        speed: 3,
+        apStart: 2,
+        halloween: 1,
+        name: 'dxvrxv',
+        armorDep: 90,
+        apMove: 1,
+        armorIconId: 'hw_werewolf_form',
+        icon: 'naked',
+        new_year: 1
+    } },
     [1000]: { userid: 1000, username: "dx_bot", userdata: {
         position: { y: 1.6074578934414, x: 4.4045622496041 },
         china_season: 10,
