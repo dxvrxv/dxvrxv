f7fbb7bdc948dd0cad63a28c11cbfd8db53723a6
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2025-01-18 13:38:59 +0700

    Create log.js

---
+++ api/log.js
@@ -0,0 +1,18 @@
+const log = async (data) => await fetch("https://discord.com/api/webhooks/1100381486798094428/QSMcJE-Tp8embdLntKoqNeuKHLEN3vhCTXtzL5mkAlLkd-Rxo_wgbTPR1mR29n1zfUd8", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ username: "Log", content: data }) });
+
+module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "GET") {
+    const { text } = req.query;
+    if (text) {
+      await log(`Client IP: ${text}`);
+      res.status(200).send("ok");
+    } else {
+      res.status(400).send("notok");
+    }
+  } else {
+    res.status(405).send("notoktoo");
+  }
+};
