ce0bdcb600878f7cd969bd2e530bec890c4fb803
dxvrxv <119617466+dxvrxv@users.noreply.github.com>
2026-01-21 02:16:32 +0700

    Update settings.js

--- api/settings.js
+++ api/settings.js
@@ -1,1 +1,26 @@
+module.exports = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
 
+  res.end(JSON.stringify({
+    data: {
+      servers: [
+        {
+          host: "wss://6ea881e74575.ngrok-free.app",
+          locale: "",
+          port_tcp: 443,
+          primary_tcp_port: 443,
+          visible_name: "dayr-global",
+          network_protocol: 123,
+          encryption_key: "",
+          minimum_version_ios: 711,
+          minimum_version_android: 711,
+          online: 1,
+          time_updated: Math.floor(Date.now() / 1000),
+          delta_updated: 0,
+          is_serialize_int: false
+        }
+      ]
+    },
+    result: 1
+  }));
+};
